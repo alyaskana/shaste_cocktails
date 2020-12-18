64b91491cc3ad315bf2fe36d6d46157c3cb38605
Alina Bychkova <ialina240200@gmail.com>
2020-12-19 00:47:53 +0300

    Generate tasted_cocktails model

---
+++ app/models/tasted_cocktail.rb
@@ -0,0 +1,4 @@
+class TastedCocktail < ApplicationRecord
+  belongs_to :user
+  belongs_to :cocktail
+end
---
+++ test/models/tasted_cocktail_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TastedCocktailTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
