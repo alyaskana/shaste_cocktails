2d1fd9d3b355895d9638b93d7b6f49ddbfc4d902
Alina Bychkova <ialina240200@gmail.com>
2021-06-21 13:57:20 +0300

    Fix user ingredients sorting

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   has_many :cocktails, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :user_ingredients, dependent: :destroy
-  has_many :ingredients, :through => :user_ingredients
+  has_many :ingredients, -> { order 'user_ingredients.created_at' }, :through => :user_ingredients
 
   has_many :favorites, dependent: :destroy
   has_many :favorited_cocktails, :through => :favorites, :source => :cocktail
